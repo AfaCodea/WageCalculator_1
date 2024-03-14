2fe029821479b9154082b883e9a6a3b468de6c67
AgilFrasunza26 <130188087+AfaCodea@users.noreply.github.com>
2024-03-14 11:17:07 +0700

    done revisi

--- Form1.vb
+++ Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
-        ' deklarasi variabel
+        ' deklarasi variabel'
         Dim hours As Integer
         Dim payrate As Integer
         Dim basic As Integer
